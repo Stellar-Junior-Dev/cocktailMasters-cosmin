8f97d04fd5fe9014d63cfa52710b2b9eb7af8420
Cosmin <cosmin.uliniuc@gmail.com>
2023-04-26 16:43:31 +0300

    1stPage

--- script.js
+++ script.js
@@ -24,58 +24,148 @@ let data = {
   cocktailList: [mojito, ginLemon, oldFashioned],
 };
 
+let longIslandTea = {
+  image:
+    "https://www.thecocktaildb.com/images/media/drink/nkwr4c1606770558.jpg",
+  favorite: false,
+  name: "Long Island Tea",
+};
+let negroni = {
+  image:
+    "https://www.thecocktaildb.com/images/media/drink/qgdu971561574065.jpg",
+  favorite: true,
+  name: "Negroni",
+};
+let whiskeySour = {
+  image:
+    "https://www.thecocktaildb.com/images/media/drink/hbkfsh1589574990.jpg",
+  favorite: true,
+  name: "Whiskey Sour",
+};
+
+let dataTwo = {
+  categoryName: "Latest drinks",
+  cocktailList: [longIslandTea, negroni, whiskeySour],
+};
+
+let dryMartini = {
+  image:
+    "https://www.thecocktaildb.com/images/media/drink/6ck9yi1589574317.jpg",
+  favorite: false,
+  name: "Dry Martini",
+};
+let daiquiry = {
+  image:
+    "https://www.thecocktaildb.com/images/media/drink/mrz9091589574515.jpg",
+  favorite: true,
+  name: "Daiquiry",
+};
+let margarita = {
+  image:
+    "https://www.thecocktaildb.com/images/media/drink/5noda61589575158.jpg",
+  favorite: true,
+  name: "Margarita",
+};
+
+let dataThree = {
+  categoryName: "Random drinks",
+  cocktailList: [dryMartini, daiquiry, margarita],
+};
+
+const drinksArray = [data, dataTwo, dataThree];
+console.log(drinksArray);
+
 window.addEventListener("load", renderSection());
 
 function renderSection() {
-  // pentru obiectul "data" afisati categoria completa.
+  //Append shit
+  for (obj of drinksArray) {
+    const body = document.querySelector("body");
 
-  const drinkType = document.createElement("h3");
-  drinkType.textContent = data.categoryName;
-  // console.log(drinkType.textContent);
-  const titleName = document.querySelector(".title-name");
-  titleName.appendChild(drinkType);
-  let cards;
-  cards = renderCard();
+    //build sections :
+
+    const section = document.createElement("section");
+    section.className = "group";
+
+    //build header
+
+    const header = document.createElement("header");
+
+    //build div title
+
+    const divTitle = document.createElement("div");
+    divTitle.className = "title-name";
+
+    //build div-buttons
+
+    const divBtn = document.createElement("div");
+    divBtn.className = "view-all-button";
+
+    //Build button
+    const button = document.createElement("button");
+    button.innerHTML = `view all <ion-icon name="chevron-forward-outline"></ion-icon>`;
+
+    //build divDrinks
+    const divDrinks = document.createElement("div");
+    divDrinks.className = "row-drinks";
+
+    const drinkType = document.createElement("h3");
+
+    drinkType.textContent = obj.categoryName;
+    divTitle.appendChild(drinkType);
+    divBtn.appendChild(button);
+    header.appendChild(divTitle);
+    header.appendChild(divBtn);
+
+    section.appendChild(header);
+    body.appendChild(section);
+    const main = document.createElement("main");
+    const divRow = document.createElement("div");
+    divRow.className = "row-drinks";
+
+    for (item of obj.cocktailList) {
+      let card = renderCard(item);
+      divRow.appendChild(card);
+    }
+    main.appendChild(divRow);
+    section.appendChild(main);
+  }
 }
 
-function renderCard() {
-  for (let i = 0; i < data.cocktailList.length; i++) {
-    const test = document.getElementById("popularDrinks");
+function renderCard(item) {
+  const divCard = document.createElement("div");
+  divCard.classList.add("card");
 
-    const divCard = document.createElement("div");
-    divCard.classList.add("card");
+  const divCardImage = document.createElement("div");
+  divCardImage.classList.add("card-image");
 
-    const divCardImage = document.createElement("div");
-    divCardImage.classList.add("card-image");
+  const image = document.createElement("img");
+  image.src = item.image;
 
-    const image = document.createElement("img");
-    image.src = data.cocktailList[i].image;
+  const divCardTitle = document.createElement("div");
+  divCardTitle.classList.add("card-title");
 
-    const divCardTitle = document.createElement("div");
-    divCardTitle.classList.add("card-title");
+  const divCenter = document.createElement("div");
+  divCenter.classList.add("center");
 
-    const divCenter = document.createElement("div");
-    divCenter.classList.add("center");
+  const paragraph = document.createElement("p");
+  paragraph.textContent = item.name;
 
-    const paragraph = document.createElement("p");
-    paragraph.textContent = data.cocktailList[i].name;
-
-    const divCardIcon = document.createElement("div");
-    divCardIcon.classList.add("card-icon");
-    const icon = document.createElement("ion-icon");
-    if (data.cocktailList[i].favorite === true) {
-      icon.setAttribute("name", "heart");
-    } else {
-      icon.setAttribute("name", "heart-outline");
-    }
-
-    divCardIcon.appendChild(icon);
-    divCenter.appendChild(paragraph);
-    divCardTitle.appendChild(divCenter);
-    divCardImage.appendChild(image);
-    divCard.appendChild(divCardImage);
-    divCard.appendChild(divCardTitle);
-    divCard.appendChild(divCardIcon);
-    test.appendChild(divCard);
+  const divCardIcon = document.createElement("div");
+  divCardIcon.classList.add("card-icon");
+  const icon = document.createElement("ion-icon");
+  if (item.favorite === true) {
+    icon.setAttribute("name", "heart");
+  } else {
+    icon.setAttribute("name", "heart-outline");
   }
+
+  divCardIcon.appendChild(icon);
+  divCenter.appendChild(paragraph);
+  divCardTitle.appendChild(divCenter);
+  divCardImage.appendChild(image);
+  divCard.appendChild(divCardImage);
+  divCard.appendChild(divCardTitle);
+  divCard.appendChild(divCardIcon);
+  return divCard;
 }
